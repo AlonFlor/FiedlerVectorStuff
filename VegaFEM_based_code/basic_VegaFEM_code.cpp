0f615528df9d04d3e8e4f7ccd97b9ef3c4a6c241
AlonFlor <43430118+AlonFlor@users.noreply.github.com>
2023-12-07 19:14:49 -0500

    enable other constitutive models

--- VegaFEM_based_code/basic_VegaFEM_code.cpp
+++ VegaFEM_based_code/basic_VegaFEM_code.cpp
@@ -9,8 +9,26 @@
 #include <fstream>
 #include <sstream>
 #include "volumetricMeshLoader.h"
+
 #include "corotationalLinearFEM.h"
 #include "corotationalLinearFEMStencilForceModel.h"
+
+#include "neoHookeanIsotropicMaterial.h"
+//#include "MooneyRivlinIsotropicMaterial.h"
+#include "isotropicHyperelasticFEM.h"
+#include "isotropicHyperelasticFEMStencilForceModel.h"
+
+#include "linearFEMStencilForceModel.h"
+
+#include "massSpringSystem.h"
+#include "massSpringSystemFromTetMesh.h"
+#include "massSpringStencilForceModel.h"
+
+#include "StVKElementABCDLoader.h"
+#include "StVKFEM.h"
+#include "StVKStencilForceModel.h"
+
+
 #include "forceModelAssembler.h"
 #include "generateMassMatrix.h"
 #include "implicitBackwardEulerSparse.h"
@@ -24,7 +42,7 @@ int print_frame(std::string file_prefix, std::string header, std::string suffix,
    fprintf(fp, "%s", header.data());
    for(int i=0; i<r; i++){
       fprintf(fp, "%f ",u_original[i] + u[i]);
-      if(i>0 && (i+1)%3==0){
+      if(i%3==2){
          fprintf(fp, "\n");
       }
    }
@@ -75,19 +93,74 @@ std::vector<std::string> get_header_and_suffix(int num_vertices, int num_faces, 
    return result;
 }
 
-double run_one_sim(char* inputFilename, int attempt_number){
+int get_start_index_of_vertex(double x, double y, double z, double* u_original, int r){
+   //get index of x-value of vertex from dragon model or its reorderings. Needed because the same vertex could be in different locations in different models.
+   double dist_sq = 0.;
+   double min_dist = 1000000.;
+   int min_dist_start_index = -1;
+   for(int i=0; i<r; ++i){
+      if(i%3==0)dist_sq += (u_original[i] - x)*(u_original[i] - x);
+      if(i%3==1)dist_sq += (u_original[i] - y)*(u_original[i] - y);
+      if(i%3==2){
+         dist_sq += (u_original[i] - z)*(u_original[i] - z);
+         if(dist_sq < min_dist){
+            min_dist = dist_sq;
+            min_dist_start_index = i-2;
+         }
+         dist_sq = 0.;
+      }
+   }
+   return min_dist_start_index;
+}
+
+double run_one_sim(char* inputFilename, int attempt_number, std::string constitutive_model){
    std::string output_file_prefix = inputFilename;
-   output_file_prefix = output_file_prefix.substr(0,output_file_prefix.size()-4);
+   output_file_prefix = output_file_prefix.substr(0,output_file_prefix.size()-4) + "_" + constitutive_model;
    output_file_prefix += "_" + std::to_string(attempt_number);
    VolumetricMesh * volumetricMesh = VolumetricMeshLoader::load(inputFilename);
    if (volumetricMesh == NULL) printf("Error: failed to load mesh.\n");
    else printf("Success. Number of vertices: %d . Number of elements: %d .\n", volumetricMesh->getNumVertices(), volumetricMesh->getNumElements());
 
-   CorotationalLinearFEM * deformableModel = new CorotationalLinearFEM(volumetricMesh);
-
    // Create the class to compute internal elastic forces and stiffness matrices
    // on each mesh element.
-   StencilForceModel * stencilForceModel = new CorotationalLinearFEMStencilForceModel(deformableModel);
+   IsotropicHyperelasticFEM * IsotropicHyperelasticDeformableModel;
+   IsotropicMaterial * material;
+   MassSpringSystem * MassSpringDeformableModel;
+   StVKElementABCD * precomputedIntegrals;
+   StVKFEM * StVKDeformableModel;
+   CorotationalLinearFEM * CorotationalLinearDeformableModel;
+   TetMesh* tetMesh = (TetMesh*)volumetricMesh;
+
+   StencilForceModel * stencilForceModel;
+   StencilForceModel * stencilForceModel_1;
+   std::cout<<constitutive_model.data()<<std::endl;
+   if(constitutive_model.compare("neoHookean")==0){
+      material = new NeoHookeanIsotropicMaterial(tetMesh);
+      IsotropicHyperelasticDeformableModel = new IsotropicHyperelasticFEM(tetMesh, material);
+      stencilForceModel = new IsotropicHyperelasticFEMStencilForceModel(IsotropicHyperelasticDeformableModel);
+   }/*else if(constitutive_model.compare("MR")==0){
+      material = new MooneyRivlinIsotropicMaterial(tetMesh);
+      IsotropicHyperelasticDeformableModel = new IsotropicHyperelasticFEM(tetMesh, material);
+      stencilForceModel = new IsotropicHyperelasticFEMStencilForceModel(IsotropicHyperelasticDeformableModel);
+   }*/else if(constitutive_model.compare("massSpring")==0){
+      MassSpringSystemFromTetMesh massSpringSystemFromTetMesh;
+      massSpringSystemFromTetMesh.GenerateMassSpringSystem(tetMesh, &MassSpringDeformableModel, 1.0, 50000., 0.01);
+      stencilForceModel = new MassSpringStencilForceModel(MassSpringDeformableModel);
+   }else if(constitutive_model.compare("StVK")==0){
+      unsigned int loadingFlag = 0; // 0 = use the low-memory version, 1 = use the high-memory version
+      precomputedIntegrals = StVKElementABCDLoader::load(volumetricMesh, loadingFlag);
+      StVKDeformableModel = new StVKFEM(volumetricMesh, precomputedIntegrals);
+      stencilForceModel = new StVKStencilForceModel(StVKDeformableModel);
+   }else if(constitutive_model.compare("corotationalLinear")==0){
+      CorotationalLinearDeformableModel = new CorotationalLinearFEM(volumetricMesh);
+      stencilForceModel = new CorotationalLinearFEMStencilForceModel(CorotationalLinearDeformableModel);
+   }else if(constitutive_model.compare("linear")==0){
+      CorotationalLinearDeformableModel = new CorotationalLinearFEM(volumetricMesh);
+      stencilForceModel_1 = new CorotationalLinearFEMStencilForceModel(CorotationalLinearDeformableModel);
+      stencilForceModel = new LinearFEMStencilForceModel(stencilForceModel_1);
+   }
+
+
    // Create the class to connect the deformable model to the integrator.
    // ForceModelAssembler automatically computes in parallel if Intel TBB is enabled.
    ForceModel * forceModel = new ForceModelAssembler(stencilForceModel);
@@ -111,10 +184,18 @@ double run_one_sim(char* inputFilename, int attempt_number){
    SparseMatrix * massMatrix;
    // Create consistent (non-lumped) mass matrix.
    GenerateMassMatrix::computeMassMatrix(volumetricMesh, &massMatrix, true);
-   // constraining vertices 4, 10, 14 (constrained DOFs are specified 0-indexed):
    int numConstrainedDOFs = 0;//9;
    int constrainedDOFs[0] = {};
+   /*// constraining vertices 4, 10, 14 (constrained DOFs are specified 0-indexed):
+   int constrainedDOF_0 = get_start_index_of_vertex(1.22424, -2.25693, 1.43553, u_original, r);
+   int constrainedDOF_1 = get_start_index_of_vertex(1.19529, -2.30061, 1.44579, u_original, r);
+   int constrainedDOF_2 = get_start_index_of_vertex(1.15391, -2.35621, 1.40962, u_original, r);
    //int constrainedDOFs[9] = { 12, 13, 14, 30, 31, 32, 42, 43, 44 };
+   int constrainedDOFs[9] = { constrainedDOF_0, constrainedDOF_0+1, constrainedDOF_0+2, constrainedDOF_1, constrainedDOF_1+1, constrainedDOF_1+2,
+                              constrainedDOF_2, constrainedDOF_2+1, constrainedDOF_2+2 };
+   //std::cout<<std::to_string(constrainedDOFs[0])+" "<<std::to_string(constrainedDOFs[1])+" "<<std::to_string(constrainedDOFs[2])+" ";
+   //std::cout<<std::to_string(constrainedDOFs[3])+" "<<std::to_string(constrainedDOFs[4])+" "<<std::to_string(constrainedDOFs[5])+" ";
+   //std::cout<<std::to_string(constrainedDOFs[6])+" "<<std::to_string(constrainedDOFs[7])+" "<<std::to_string(constrainedDOFs[8])<<std::endl;*/
    // (tangential) Rayleigh damping
    double dampingMassCoef = 0.0; // "underwater"-like damping (here turned off)
    double dampingStiffnessCoef = 0.01; // (primarily) high-frequency damping
@@ -133,8 +214,12 @@ double run_one_sim(char* inputFilename, int attempt_number){
       u[i]=0.;
    }
 
+   int force_index = get_start_index_of_vertex(1.3844, -2.18075, 1.35392, u_original, r) + 1;
+   //std::cout<<"force_index "<<force_index<<std::endl;
+   //std::cout<<"u[force_index] "<<u_original[force_index]<<std::endl;
+
    double time_elapsed = 0.;
-   int numTimesteps = 10;
+   int numTimesteps = 50;
    FILE* fp;
    for(int i=0; i<numTimesteps; i++){
       //print the current time step
@@ -148,7 +233,7 @@ double run_one_sim(char* inputFilename, int attempt_number){
       implicitBackwardEulerSparse->SetExternalForcesToZero();
       if (i == 0){ // set some force at the first timestep
          for(int j=0; j<r; j++)f[j] = -50.; // set forces for every node
-         //f[37] = -500; // apply force of -500 N to vertex 12, in y-direction, 3*12+1 = 37
+         //f[force_index] = -500; // apply force of -500 N to vertex 12, in y-direction, 3*12+1 = 37
          implicitBackwardEulerSparse->SetExternalForces(f);
       }
       implicitBackwardEulerSparse->DoTimestep();
@@ -167,9 +252,28 @@ double run_one_sim(char* inputFilename, int attempt_number){
    //print time
    std::cout << time_elapsed << std::endl;
 
+   //free memory
    free(u_original);
    free(u);
    free(f);
+   if(constitutive_model.compare("neoHookean")==0){//} || constitutive_model.compare("MR")==0){
+      delete material;
+      delete IsotropicHyperelasticDeformableModel;
+   }else if(constitutive_model.compare("massSpring")==0){
+      delete MassSpringDeformableModel;
+   }else if(constitutive_model.compare("StVK")==0){
+      //delete precomputedIntegrals;      //Not sure why, but freeing the memory from precomputedIntegrals leads to a segfault.
+      delete StVKDeformableModel;
+   }else if(constitutive_model.compare("corotationalLinear")==0){
+      delete CorotationalLinearDeformableModel;
+   }else if(constitutive_model.compare("linear")==0){
+      delete CorotationalLinearDeformableModel;
+      delete stencilForceModel_1;
+   }
+   delete stencilForceModel;
+   delete forceModel;
+   delete implicitBackwardEulerSparse;
+   delete volumetricMesh; //I think this should also delete tetMesh.
 
    return time_elapsed;
 }
@@ -178,8 +282,9 @@ double run_one_sim(char* inputFilename, int attempt_number){
 int main(int argc, char *argv[])
 {
    int num_times = 0;
-   for(int i=1; i<argc; ++i){
-      if(i%2==1){
+   std::string constitutive_model = argv[1];
+   for(int i=2; i<argc; ++i){
+      if(i%2==0){
          num_times=std::stoi(argv[i]);
       }else{
          printf("Simulating %s for %d times.\n", argv[i], num_times);
@@ -188,7 +293,7 @@ int main(int argc, char *argv[])
          std::vector<double> timings;
          double average_time = 0.;
          for(int j=0; j<num_times; ++j){
-            double time_elapsed_one_run = run_one_sim(argv[i], j);
+            double time_elapsed_one_run = run_one_sim(argv[i], j, constitutive_model);
             timings.push_back(time_elapsed_one_run);
             average_time += time_elapsed_one_run;
          }
@@ -204,10 +309,10 @@ int main(int argc, char *argv[])
          std_dev_time = sqrt(std_dev_time);
 
          //print timings
-         char file_path[50];
+         char file_path[70];
          std::string file_name = argv[i];
-         file_name = file_name.substr(0,file_name.size()-4);
-         sprintf(file_path, "output/%s_timings.txt", file_name.data());
+         file_name = file_name.substr(0,file_name.size()-4) + "_" + constitutive_model;
+         sprintf(file_path, "output/timings/%s_timings.txt", file_name.data());
          FILE* fp = fopen(file_path, "w");
          for(int j=0; j<num_times; ++j){
             fprintf(fp, "%lf\n", timings[j]);
